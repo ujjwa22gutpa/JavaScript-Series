5ac4e49600fcdcf44e7b8e6e098eb917a119d094
ujjwa22gutpa <ujjwalgupta1072@gmail.com>
2025-07-07 15:26:10 +0000

    About Arrays

---
+++ 02_basics/Array.js
@@ -0,0 +1,29 @@
+const arr=[0,1,2,3,4,5,6];
+// console.log(arr);
+// // console.log(arr.toString());
+// arr.push(10)
+// arr.push(11);
+// console.log(arr);
+// arr.pop()
+// arr.pop();
+// console.log(arr);
+// arr.unshift(23);
+// arr.unshift(56);
+// console.log(arr);
+// arr.shift()
+// arr.shift()
+// console.log(arr);
+// console.log(arr.includes(4));
+// console.log(arr.indexOf(3));
+
+
+//slice and splice
+
+const arr1= [2,3,4,5,6];
+console.log('A',arr1);
+console.log(arr1.slice(1,4));
+console.log('B',arr1)
+
+const arr2=[4,5,6,7,8];
+console.log(arr2.splice(1,4));
+console.log('C',arr2);
---
+++ 02_basics/Arrays2.js
@@ -0,0 +1,17 @@
+// console.log(Array.isArray("ujjwal"));
+// console.log(Array.from("ujjwal"));
+
+// const num1=22;
+// const num2="hiii!!"
+// const num3=true;
+
+// console.log(Array.of(num1,num2,num3));
+
+
+
+const marvel_heros=['iron','thor','apna captain']
+const dc_heros=['superman','batii','spidyy']
+const all_heros=[...dc_heros,...marvel_heros];
+console.log(all_heros)
+
+
