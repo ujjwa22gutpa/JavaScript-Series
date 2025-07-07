9ab084256071f986926682cfaec5edbd4285e5c2
ujjwa22gutpa <ujjwalgupta1072@gmail.com>
2025-07-07 13:45:54 +0000

    About Strings Methods

---
+++ NumMath.js
@@ -0,0 +1,32 @@
+const num =1000000;
+const score=134.60908;
+// console.log(score.toFixed(3));
+// console.log(num.toLocaleString())
+// console.log(num.toLocaleString())
+// console.log(num.toString());
+// console.log(num.toPrecision(4));
+
+//****************Math ********* */
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(-3.545434));
+// console.log(Math.min(-3,6,3,9,0));
+// console.log(Math.max(32,5,676,24,2));
+// console.log(Math.ceil(-3.45));
+// console.log(Math.floor(-3.5645));
+
+console.log(Math.random());
+//console.log(Math.floor(Math.random()*10)+1);
+for(let i =0;i<11;i++){
+    console.log(Math.floor(Math.random()*10)+1);
+}
+
+
+// let min =5;
+// let max=15;
+// //console.log(Math.floor(Math.random()*(max-min+1))+min)
+// for(let i =0;i<11;i++){
+// console.log(Math.floor(Math.random()*(max-min+1))+min)
+// }
+
