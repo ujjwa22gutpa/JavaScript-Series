49d3d719826767c5a9f43efb67ac510e8666ab67
ujjwa22gutpa <ujjwalgupta1072@gmail.com>
2025-07-07 05:43:51 +0000

    ConversionOfDatatypes

---
+++ 01_basics/03_typeconversion.js
@@ -0,0 +1,29 @@
+// let isNumber=33;
+let isNumber="33";
+
+// console.log(typeof isNumber);
+let con=Number(isNumber);
+//console.log(typeof con);
+
+// Number()
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true=1  false= 0
+// 
+
+
+
+let isLoggedIn=1;
+let isLogged="";
+let LoggedIn=true;
+let BooleanIsLogged=Boolean(isLoggedIn);
+let BooleanLoggedIn=Boolean(isLogged);
+let IsLoggedIn=Boolean(LoggedIn);
+console.log(BooleanIsLogged)
+console.log(BooleanLoggedIn)
+console.log(IsLoggedIn)
+
+//Boolean()
+//1=> true  
+// " "=> false
+// "ujjwal"=> true
