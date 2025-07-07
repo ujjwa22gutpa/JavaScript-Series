6c19200c3217f0db09ec78c442a610b15bb246ff
ujjwa22gutpa <ujjwalgupta1072@gmail.com>
2025-07-07 14:36:07 +0000

    Dates and Time

---
+++ 01_basics/datesTime.js
@@ -0,0 +1,25 @@
+// const date= new Date();
+
+//const date=new Date(2023,6,2,6,9);
+
+ const date=new Date("03-13-2003");
+
+// //console.log(date);
+//  console.log(date.toDateString())
+// // console.log(date.toISOString())
+// // console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+// // console.log(date.toLocaleString())
+// // console.log(date.toString())
+
+
+
+const myTimeStamp= Date.now();
+console.log(myTimeStamp);
+
+console.log(date.getTime());
+console.log(Date.now()/1000);
+console.log(Math.floor(Date.now()/1000));
+
+
+
